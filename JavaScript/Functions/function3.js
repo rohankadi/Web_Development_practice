d713d67756d550cabf247691949c43f9c52d5432
Rohan Kadi <jeevarhn.rk174@gmail.com>
2023-12-21 19:48:50 +0530

    Add files via upload

---
+++ JavaScript/Functions/function3.js
@@ -0,0 +1,18 @@
+function myfunction(a, b, c, d,) {
+        return (a + b + c + d) / 4
+    }
+
+    console.log(myfunction(4, 5, 6, 7))
+
+ //or 2nd type
+   /* let num1 = 5;
+    let num2 = 4;
+    let num3 = 6;
+    let num4 = 7;
+
+
+    function mean(num1, num2, num3, num4) {
+        return (num1 + num2 + num3 + num4) / 4;
+    }
+
+    console.log(mean(num1, num2, num3, num4));*/
