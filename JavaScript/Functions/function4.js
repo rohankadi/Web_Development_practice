3124162277cccf6e471b83057d6b60a641db1b61
Rohan Kadi <jeevarhn.rk174@gmail.com>
2023-12-21 19:48:36 +0530

    Add files via upload

---
+++ JavaScript/Functions/function4.js
@@ -0,0 +1,14 @@
+//Array Sorting
+let d = [51, 22, 3, 14, 53, 65, 98, 85, 70]
+
+d.sort(function myfun(a, b) {
+    return a - b
+})
+console.log(d);
+
+// let accend = (a, b) => {
+//     return a - b
+// }
+
+// console.log(d.sort(accend));
+
