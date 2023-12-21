1e005ff8e008effb8db48a726c6b60d7f0d9f084
Rohan Kadi <jeevarhn.rk174@gmail.com>
2023-12-21 19:48:14 +0530

    Add files via upload

---
+++ JavaScript/Functions/function5.js
@@ -0,0 +1,11 @@
+//degree Celcius to Farhenheit
+var x = 65;
+//01
+var y = (x * 9 / 5) + 32
+console.log(y)
+
+//02
+// function myfun(x){
+//    return (x * 9/5) + 32
+// }
+// console.log(myfun(x));
