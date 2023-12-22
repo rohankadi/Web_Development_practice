c477847af9a633fbcdd97abdf00c2b9aaac63ff6
Rohan Kadi <jeevarhn.rk174@gmail.com>
2023-12-22 21:02:41 +0530

    Add files via upload

---
+++ JavaScript/Js_ApnaCollege/practice_08_printEvenNumber.js
@@ -0,0 +1,17 @@
+
+/* rough printing all numbers
+for(let i = 1; i < 10; i++){
+    console.log(i);
+}
+
+//reverse order
+for(let i = 10; i>0; i--){
+    console.log(i);
+}
+*/
+
+for(let i=1; i<=100; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
