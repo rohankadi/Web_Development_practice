ff4ca331beae8d1d612de9d85be9a468ced028ba
Rohan Kadi <jeevarhn.rk174@gmail.com>
2023-12-22 20:58:07 +0530

    Add files via upload

---
+++ JavaScript/Js_ApnaCollege/practice_01.js
@@ -0,0 +1,13 @@
+const product = {
+    title : "Ball Pen",
+    rating : 4,
+    isDeal : true,
+    price : 270,
+    offer : 5
+}
+
+console.log(typeof(product));
+console.log(product);
+
+console.log("Title is : " + product.title);
+console.log("Dhamaka Offer is : " + product.offer + "%");
