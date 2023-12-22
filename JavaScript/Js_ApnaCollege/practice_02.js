223c1145efbf789c72e5d235e0c0a5c69685f1ae
Rohan Kadi <jeevarhn.rk174@gmail.com>
2023-12-22 20:58:57 +0530

    Add files via upload

---
+++ JavaScript/Js_ApnaCollege/practice_02.js
@@ -0,0 +1,12 @@
+const profile = {
+    username : "Shraddha Kapra",
+    isFollow : true,
+    posts : 195,
+    followers : 569000,
+    following : 4,
+    bio : "Apna College | Ex-Microsoft, DRDO"
+}
+
+console.log(profile);
+console.log(profile.username);
+console.log(profile.isFollow);
